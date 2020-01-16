d243eb88ba4bc2dfc1012e5354fb2e950e302554
Yuri Kotliarov <yuri.kotliarov@nih.gov>
2020-01-16 15:25:21 -0500

    Fixed PDF save

--- R/IFN_signature_analysis/logistic_regr_4flu.r
+++ R/IFN_signature_analysis/logistic_regr_4flu.r
@@ -34,22 +34,19 @@ dn.fig = file.path(PROJECT_DIR, "figure_generation/IFN26")
 
 fit1 = glm(Response ~ IFN26 + Study, family="binomial", data=DF)
 summary(fit1)
-# fit1.coef=coef(summary(fit1))
-# fit1.coef
 
 fn.fig = file.path(dn.fig, "4flu-logistic-regression-1pred_effects-IFN26-bars")
 p = summary(fit1)$coef[2,4]
 main=sprintf("p = %0.3g", p)
 ylab = "Probability of high responder"
 eff = Effect("IFN26", fit1, xlevels=list(IFN26=seq(-1,1,0.5)))
+
+pdf(paste0(fn.fig, ".pdf"))
 with(summary(eff), {
   barplot(effect, main=main,  width=1, space=0, ylim=c(0,1), las=1,
           xlab="IFN-I-DCact score", ylab=ylab)
-  plotCI(seq(0.5,4.5,1), effect, uiw=upper-effect, liw=effect-lower, add=T, gap=0, type="p", pch=29)
+  plotCI(seq(0.5,4.5,1), effect, uiw=upper-effect, liw=effect-lower, add=T, gap=0, type="p", pch=NA)
 })
-dev.copy(png, paste0(fn.fig, ".png"))
-dev.off()
-dev.copy(pdf, paste0(fn.fig, ".pdf"))
 dev.off()
 
 
@@ -57,22 +54,18 @@ dev.off()
 
 fit1 = glm(Response ~ CD38 + IFN26 + Study, family="binomial", data=DF)
 summary(fit1)
-# fit1.coef=coef(summary(fit1))
-# fit1.coef
 
 fn.fig = file.path(dn.fig, "4flu-logistic-regression-2pred_effects-CD38-bars")
 p = summary(fit1)$coef[2,4]
 main=sprintf("p = %0.3g", p)
 ylab = "Probability of high responder"
 eff = Effect("CD38", fit1, xlevels=list(CD38=seq(-1,1,0.5)))
+pdf(paste0(fn.fig, ".pdf"))
 with(summary(eff), {
   barplot(effect, main=main,  width=1, space=0, ylim=c(0,1), las=1,
           xlab="TGSig score", ylab=ylab)
   plotCI(seq(0.5,4.5,1), effect, uiw=upper-effect, liw=effect-lower, add=T, gap=0, type="p", pch=NA)
 })
-dev.copy(png, paste0(fn.fig, ".png"))
-dev.off()
-dev.copy(pdf, paste0(fn.fig, ".pdf"))
 dev.off()
 
 fn.fig = file.path(dn.fig, "4flu-logistic-regression-2pred_effects-IFN26-bars")
@@ -80,13 +73,11 @@ p = summary(fit1)$coef[3,4]
 main=sprintf("p = %0.3g", p)
 ylab = "Probability of high responder"
 eff = Effect("IFN26", fit1, xlevels=list(IFN26=seq(-1,1,0.5)))
+pdf(paste0(fn.fig, ".pdf"))
 with(summary(eff), {
   barplot(effect, main=main,  width=1, space=0, ylim=c(0,1), las=1,
           xlab="IFN-I-DCact score", ylab=ylab)
   plotCI(seq(0.5,4.5,1), effect, uiw=upper-effect, liw=effect-lower, add=T, gap=0, type="p", pch=NA)
 })
-dev.copy(png, paste0(fn.fig, ".png"))
-dev.off()
-dev.copy(pdf, paste0(fn.fig, ".pdf"))
 dev.off()
 
